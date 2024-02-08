f26aabf7f42315bb0c82ed072924adcdc2122eb8
Jodie Mullins <jmullins2460@gmail.com>
2024-02-08 11:58:48 -0500

    counter

---
+++ inter.js
@@ -0,0 +1,38 @@
+// document.getElementById("count").innerText = 5
+
+let count = 0
+console.log(count)
+
+
+let myAge = 29
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+console.log(myDogAge)
+
+// Create a variable, bonusPoints. Initialize it as 50.
+// let bonusPoints = 50
+// console.log(bonusPoints)
+// // Increase it to 100
+// bonusPoints = bonusPoints + bonusPoints
+// console.log(bonusPoints)
+
+// // Decrease it down to 25
+// bonusPoints = bonusPoints / 4
+// console.log(bonusPoints)
+
+// // Increase to 70
+// bonusPoints = bonusPoints + 45
+// console.log(bonusPoints)
+
+function increment() {
+    console.log("The button was clicked")
+
+}
+
+function galaxyAnswer() {
+    console.log("42")
+
+}
+
+
